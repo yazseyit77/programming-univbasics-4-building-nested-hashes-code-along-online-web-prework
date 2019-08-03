d8d1b22fcdbf4df1f089a3ebe6c61c1228d8dd45
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-03 17:59:00 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,6 +12,7 @@ def third_challenge
       :hero => {
         :name => "Romeo",
         :age => "15"
+        :status => "alive"
         },
       :hero_friends => []
    },
@@ -27,6 +28,7 @@ def third_challenge
       :heroine => {
         :name => "Juliet",
         :age => "15"
+        :status => "alive"
         },
       :heroine_friends => []
    }
